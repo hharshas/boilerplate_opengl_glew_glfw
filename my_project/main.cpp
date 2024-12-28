9c61fc871aa0d5abe3debd665b4f881588bd67f2
harsh singh <22je0388@iitism.ac.in>
2024-12-28 14:47:47 +0530

    added glew

--- my_project/main.cpp
+++ my_project/main.cpp
@@ -1,4 +1,7 @@
+#include <GL/glew.h>
 #include <GLFW/glfw3.h>
+#include <bits/stdc++.h>
+using namespace std;
 
 int main(void)
 {
@@ -18,12 +21,23 @@ int main(void)
 
     /* Make the window's context current */
     glfwMakeContextCurrent(window);
-
+    if(glewInit() != GLEW_OK){
+        cout<<"!Error"<<endl;
+    } else {
+        cout<<"hello"<<endl;
+    }
+    cout<<glGetString(GL_VERSION)<<endl;
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
     {
         /* Render here */
         glClear(GL_COLOR_BUFFER_BIT);
+
+        glBegin(GL_TRIANGLES);
+        glVertex2f(-0.5f, -0.5f);
+        glVertex2f(0.0f, 0.5f);
+        glVertex2f(0.5f, -0.5f);
+        glEnd();
 
         /* Swap front and back buffers */
         glfwSwapBuffers(window);
